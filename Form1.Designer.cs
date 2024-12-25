ae741920c3e6b9a7ecfb5c0441f0e4bc1fcf7e68
NavozZ <navodyatheshan4@gmail.com>
2024-12-25 20:06:47 +0530

    Project Started

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(485, 205);
+            this.panel1.Location = new System.Drawing.Point(588, 338);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
             this.panel1.TabIndex = 0;
