ca24f90d15bce13eb1a0c79323183ae01559fe3f
NavozZ <navodyatheshan4@gmail.com>
2024-12-25 20:19:31 +0530

    Main Form Frontend Done

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace PUSL2019_Information_Management_and_Retrieval_
         {
             InitializeComponent();
         }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
