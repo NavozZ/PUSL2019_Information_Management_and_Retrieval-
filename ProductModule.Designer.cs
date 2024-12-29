2cc849dc2acb8164565894db133f60ec21d147a4
Warunikanchana <rpwkanchana@students.nsbm.ac.lk>
2024-12-29 21:19:48 +0530

    Product and ProductModule done.

---
+++ ProductModule.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace PUSL2019_Information_Management_and_Retrieval_
+{
+    partial class ProductModule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductModule));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picClose = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPcode = new System.Windows.Forms.Label();
+            this.txtPcode = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBarcode = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblBrand = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblReOrder = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.txtPdesc = new System.Windows.Forms.TextBox();
+            this.cboBrand = new System.Windows.Forms.ComboBox();
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.UDReOrder = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UDReOrder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGreen;
+            this.panel1.Controls.Add(this.picClose);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(882, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // picClose
+            // 
+            this.picClose.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picClose.Image = ((System.Drawing.Image)(resources.GetObject("picClose.Image")));
+            this.picClose.Location = new System.Drawing.Point(846, 19);
+            this.picClose.Name = "picClose";
+            this.picClose.Size = new System.Drawing.Size(33, 19);
+            this.picClose.TabIndex = 1;
+            this.picClose.TabStop = false;
+            this.picClose.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Module";
+            // 
+            // lblPcode
+            // 
+            this.lblPcode.AutoSize = true;
+            this.lblPcode.Location = new System.Drawing.Point(23, 105);
+            this.lblPcode.Name = "lblPcode";
+            this.lblPcode.Size = new System.Drawing.Size(146, 21);
+            this.lblPcode.TabIndex = 1;
+            this.lblPcode.Text = "Product Code :";
+            // 
+            // txtPcode
+            // 
+            this.txtPcode.Location = new System.Drawing.Point(187, 105);
+            this.txtPcode.Name = "txtPcode";
+            this.txtPcode.Size = new System.Drawing.Size(179, 30);
+            this.txtPcode.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.BackColor = System.Drawing.Color.LightGreen;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(481, 386);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 35);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(680, 386);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(84, 35);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(573, 386);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(91, 35);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "id";
+            // 
+            // lblBarcode
+            // 
+            this.lblBarcode.AutoSize = true;
+            this.lblBarcode.Location = new System.Drawing.Point(461, 108);
+            this.lblBarcode.Name = "lblBarcode";
+            this.lblBarcode.Size = new System.Drawing.Size(95, 21);
+            this.lblBarcode.TabIndex = 7;
+            this.lblBarcode.Text = "Barcode :";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(23, 163);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(117, 21);
+            this.lblDesc.TabIndex = 8;
+            this.lblDesc.Text = "Description :";
+            // 
+            // lblBrand
+            // 
+            this.lblBrand.AutoSize = true;
+            this.lblBrand.Location = new System.Drawing.Point(23, 208);
+            this.lblBrand.Name = "lblBrand";
+            this.lblBrand.Size = new System.Drawing.Size(70, 21);
+            this.lblBrand.TabIndex = 9;
+            this.lblBrand.Text = "Brand :";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(23, 254);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(106, 21);
+            this.lblCategory.TabIndex = 10;
+            this.lblCategory.Text = "Category :";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(23, 298);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(63, 21);
+            this.lblPrice.TabIndex = 11;
+            this.lblPrice.Text = "Price :";
+            // 
+            // lblReOrder
+            // 
+            this.lblReOrder.AutoSize = true;
+            this.lblReOrder.Location = new System.Drawing.Point(461, 298);
+            this.lblReOrder.Name = "lblReOrder";
+            this.lblReOrder.Size = new System.Drawing.Size(150, 21);
+            this.lblReOrder.TabIndex = 12;
+            this.lblReOrder.Text = "Re-Order Level :";
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(562, 105);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(202, 30);
+            this.txtBarcode.TabIndex = 13;
+            // 
+            // txtPdesc
+            // 
+            this.txtPdesc.Location = new System.Drawing.Point(187, 163);
+            this.txtPdesc.Name = "txtPdesc";
+            this.txtPdesc.Size = new System.Drawing.Size(577, 30);
+            this.txtPdesc.TabIndex = 14;
+            // 
+            // cboBrand
+            // 
+            this.cboBrand.FormattingEnabled = true;
+            this.cboBrand.Location = new System.Drawing.Point(187, 208);
+            this.cboBrand.Name = "cboBrand";
+            this.cboBrand.Size = new System.Drawing.Size(577, 29);
+            this.cboBrand.TabIndex = 15;
+            // 
+            // cboCategory
+            // 
+            this.cboCategory.FormattingEnabled = true;
+            this.cboCategory.Location = new System.Drawing.Point(187, 254);
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Size = new System.Drawing.Size(577, 29);
+            this.cboCategory.TabIndex = 16;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(187, 298);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(179, 30);
+            this.txtPrice.TabIndex = 17;
+            // 
+            // UDReOrder
+            // 
+            this.UDReOrder.Location = new System.Drawing.Point(617, 296);
+            this.UDReOrder.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.UDReOrder.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.UDReOrder.Name = "UDReOrder";
+            this.UDReOrder.Size = new System.Drawing.Size(147, 30);
+            this.UDReOrder.TabIndex = 18;
+            this.UDReOrder.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.UDReOrder.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // ProductModule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 575);
+            this.Controls.Add(this.UDReOrder);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.cboCategory);
+            this.Controls.Add(this.cboBrand);
+            this.Controls.Add(this.txtPdesc);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.lblReOrder);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblBrand);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblBarcode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPcode);
+            this.Controls.Add(this.lblPcode);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ProductModule";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProductModule";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UDReOrder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPcode;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBarcode;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblBrand;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblReOrder;
+        public System.Windows.Forms.ComboBox cboCategory;
+        public System.Windows.Forms.ComboBox cboBrand;
+        public System.Windows.Forms.NumericUpDown UDReOrder;
+        public System.Windows.Forms.TextBox txtPcode;
+        public System.Windows.Forms.TextBox txtBarcode;
+        public System.Windows.Forms.TextBox txtPdesc;
+        public System.Windows.Forms.TextBox txtPrice;
+        public System.Windows.Forms.Button btnSave;
+        public System.Windows.Forms.Button btnUpdate;
+    }
+}
