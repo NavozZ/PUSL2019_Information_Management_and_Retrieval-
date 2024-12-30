1d5b08765e86a4974c632536e262d66d59213187
halakshan <136452221+halakshan@users.noreply.github.com>
2024-12-30 15:06:48 +0530

    added

---
+++ SupplierModule - Copy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PUSL2019_Information_Management_and_Retrieval_
+{
+    public partial class SupplierModule : Form
+    {
+        public SupplierModule()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
