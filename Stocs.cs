ad7a97ccd47fbca0d9619660cdcf748d831dd427
sanchithaarampath <sanchitha.arampath@gmail.com>
2024-12-31 20:45:35 +0530

    Stock In Manage page  Done

--- Stocs.cs
+++ Stocs.cs
@@ -21,5 +21,15 @@ namespace PUSL2019_Information_Management_and_Retrieval_
         {
 
         }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
