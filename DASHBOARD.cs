653071be84385b436d437995ea6e4f43f42471ee
NavozZ <navodyatheshan4@gmail.com>
2025-01-03 22:15:39 +0530

    add comment for dashboard code for reslove problems

--- DASHBOARD.cs
+++ DASHBOARD.cs
@@ -22,13 +22,13 @@ namespace PUSL2019_Information_Management_and_Retrieval_
             cn = new SqlConnection(dbcon.myConnection());
         }
 
-        private void DASHBOARD_Load(object sender, EventArgs e)
+       /* private void DASHBOARD_Load(object sender, EventArgs e)
         {
             string sdate = DateTime.Now.ToShortDateString();
             lblDailySale.Text = dbcon.ExtractData("SELECT ISNULL(SUM(total),0) AS tatal FROM tbCart WHERE status LIKE 'Sold' AND sdate BETWEEN '" +sdate+ "' AND '" + sdate + "'").ToString("#,##0.00");
             lblTotalProduct.Text = dbcon.ExtractData("SELECT COUNT(*)FROM tbProduct").ToString("#,##0");
             lblStockOnHand.Text = dbcon.ExtractData("SELECT ISNULL(SUM(qty), 0) AS qty FROM tbProduct").ToString("#,##0");
             lblCriticalItems.Text = dbcon.ExtractData("SELECT COUNT(*)FROM vwCriticalItems").ToString("#,##0");
-        }
+        }*/
     }
 }
