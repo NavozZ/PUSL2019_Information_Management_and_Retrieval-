70d100d3eb78c3ec19675286c4c9323009a7d03e
NavozZ <navodyatheshan4@gmail.com>
2025-01-04 19:50:08 +0530

    Test

---
+++ MetroFramework/Controls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MetroFramework
+{
+    internal class Controls
+    {
+        internal class MetroTabControl
+        {
+            public MetroTabControl()
+            {
+            }
+
+            public object Controls { get; internal set; }
+
+            internal void SuspendLayout()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal class MetroTextBox
+        {
+        }
+    }
+}
