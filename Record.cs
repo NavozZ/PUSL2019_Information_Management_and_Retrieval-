150ee3e2cd8d49be3db43b5aaea9d5fea6c6ce6d
Ravidu-deshan <deshangunasekara2020@gmai.com>
2025-01-04 19:19:59 +0530

    Records page Top selling and Sold item Frontend Done

---
+++ Record.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PUSL2019_Information_Management_and_Retrieval_
+{
+    public partial class Record : Form
+    {
+        public Record()
+        {
+            InitializeComponent();
+        }
+
+        private void cbTopSell_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtFromTopSell_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
